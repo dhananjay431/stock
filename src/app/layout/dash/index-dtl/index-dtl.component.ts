526bd1e6b4715dedb6a6e45648934cf03099fdf1
dhananjay431 <dbel431@gmail.com>
2023-02-13 16:36:25 +0530

    20230213

--- src/app/layout/dash/index-dtl/index-dtl.component.ts
+++ src/app/layout/dash/index-dtl/index-dtl.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { HeroService } from '../../../hero.service';
-declare var XLSX: any, _: any;
+declare var XLSX: any, _: any, $: any;
 
 @Component({
   selector: 'app-index-dtl',
@@ -12,7 +12,45 @@ export class IndexDtlComponent implements OnInit {
   @Input() _data: any = {};
   oa: any = this.hs.oa;
   ob = (a: any) => a;
+  selected: any = {};
+  selected_col: any = [
+    { key: 'symbol', value: 'Symbol' },
 
+    { key: 'meta.companyName', value: 'Company Name' },
+    { key: 'identifier', value: 'identifier' },
+    { key: 'series', value: 'series' },
+    { key: 'open', value: 'Open' },
+    { key: 'dayHigh', value: 'DayHigh' },
+    { key: 'dayLow', value: 'DayLow' },
+    { key: 'lastPrice', value: 'LastPrice' },
+    { key: 'previousClose', value: 'pClose' },
+    { key: 'change', value: 'Change' },
+    { key: 'pChange', value: 'pChange' },
+    { key: 'totalTradedVolume', value: 'total Volume' },
+    { key: 'totalTradedValue', value: 'total Value' },
+
+    { key: 'yearHigh', value: 'yHigh' },
+    { key: 'ffmc', value: 'ffmc' },
+    { key: 'yearLow', value: 'YL' },
+    { key: 'nearWKH', value: 'nearWKH' },
+    { key: 'nearWKL', value: 'nearWKL' },
+    { key: 'perChange365d', value: 'perChange365d' },
+
+    { key: 'perChange30d', value: 'perChange30d' },
+
+    { key: 'meta.isFNOSec', value: 'isFNOSec' },
+    { key: 'meta.isCASec', value: 'isCASec' },
+    { key: 'meta.isSLBSec', value: 'isSLBSec' },
+    { key: 'meta.isDebtSec', value: 'isDebtSec' },
+    { key: 'meta.isSuspended', value: 'isSuspended' },
+    { key: 'meta.isETFSec', value: 'isETFSec' },
+    { key: 'meta.isDelisted', value: 'isDelisted' },
+    { key: 'meta.isin', value: 'isin' },
+  ];
+  getD(key: any, data: any) {
+    debugger;
+    return _.get(data, key) || '';
+  }
   ngOnInit(): void {}
   download(id: any) {
     let table_elt = document.getElementById(id);
@@ -123,5 +161,9 @@ export class IndexDtlComponent implements OnInit {
   }
   n(d: any) {
     return Number(d).toFixed(2);
+  }
+  open_popup(id: any, i: any) {
+    this.selected = i;
+    $(id).click();
   }
 }
