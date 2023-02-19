fd67eb3e25431687bb082b740df4e5064035b769
dhananjay431 <dbel431@gmail.com>
2023-02-19 16:34:32 +0530

    change

--- server/cron.js
+++ server/cron.js
@@ -6,27 +6,30 @@ const _server = require("./server");
 // '0/15 9-16 * * 1-5',
 
 async function dis(url) {
-  let d = await _server.get_all_nse("api/option-chain-indices?symbol=" + url);
-  fs.appendFile(
-    "./server/data/" +
-      new Date().toISOString().substr(0, 10) +
-      "_" +
-      url +
-      ".txt",
-    "\n" + JSON.stringify(d),
-    (err) => {
-      if (err) {
-        console.log("err=>", err);
-      } else {
-        console.log("DONE", new Date().toString());
+  let live = await _server.get_all_nse("/api/marketStatus");
+  if (live.marketState[0].marketStatus != "Close") {
+    let d = await _server.get_all_nse("api/option-chain-indices?symbol=" + url);
+    fs.appendFile(
+      "./server/data/" +
+        new Date().toISOString().substr(0, 10) +
+        "_" +
+        url +
+        ".txt",
+      "\n" + JSON.stringify(d),
+      (err) => {
+        if (err) {
+          console.log("err=>", err);
+        } else {
+          console.log("DONE", new Date().toString());
+        }
       }
-    }
-  );
+    );
+  }
 }
 module.exports = {
   run_cron: function (url) {
     return new CronJob(
-      "*/15 * * * *",
+      "*/1 * * * *",
       function () {
         dis(url);
       },
